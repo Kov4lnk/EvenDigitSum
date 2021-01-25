2a9852f10f08588eeac0db96e8974154843457e7
Kov4lnk <kov4lnk@gmail.com>
2021-01-25 18:56:34 +0200

    Initial commit

---
+++ src/EvenDigitSum.java
@@ -0,0 +1,19 @@
+public class EvenDigitSum {
+
+    public static int getEvenDigitSum(int number){
+        int sum = 0;
+
+
+        if (number < 0){
+            return -1;
+        }
+        while (number > 0){
+            int lastDigit = number % 10;
+            if (lastDigit % 2 == 0){
+                sum = sum + lastDigit;
+            }
+            number /= 10;
+        }
+        return sum;
+    }
+}
